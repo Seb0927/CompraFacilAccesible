00af84e60e36c24526b15d239881deee2f86e5db
Seb0927 <83418390+Seb0927@users.noreply.github.com>
2025-02-21 03:35:05 +0000

    feat: navbar implemented

---
+++ src/components/background/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import shoppingCartSvg from '../../assets/vectors/shopping_cart.svg';
+import userSvg from '../../assets/vectors/user.svg';
+import iconPng from '../../assets/images/icon.png';
+
+const Navbar = (props) => {
+  return (
+    <nav className='relative bg-blue-darkest h-16 w-full'>
+      <div className='flex items-center h-full px-4 py-3 space-x-5'>
+        <img src={iconPng} className='h-full' />
+        <h1 className='text-white text-xl font-bold'>CompraFácil</h1>
+        <hr className='h-full w-0.5 blue bg-white'/>
+        <ul className='flex items-center space-x-4'>
+          <li className='text-white text-xl'>
+            <a href='/catalogo'>Catálogo</a>
+          </li>
+          <li className='text-white text-xl'>
+            <a href='/blog'>Blog</a>
+          </li>
+          <li className='text-white text-xl'>
+            <a href='/ayuda'>Ayuda</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className='absolute right-0 top-0 h-full flex items-center px-4 py-3 space-x-5'>
+
+        {/* Justification:
+        https://stackoverflow.com/questions/21625672/what-is-an-accessible-way-to-mark-up-buttons-whose-only-content-is-an-icon */}
+        <a href="/shopping-cart" className='h-full'>
+          <img src={shoppingCartSvg} alt='Tu carrito de compras' className='h-full'/>
+        </a>
+        <button className='bg-blue-light h-full px-3 font-semibold text-lg rounded-md'>Cerrar sesión</button>
+        <a href="/user" className='h-full'>
+          <img src={userSvg} alt='Tu cuenta' className='h-full' />
+        </a>
+
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
