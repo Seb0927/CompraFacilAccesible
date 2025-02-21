41f6266a5e6f5fdba55d7650387062f12b4b281a
Seb0927 <83418390+Seb0927@users.noreply.github.com>
2025-02-21 03:35:19 +0000

    fix: background was making all non-clickable

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -1,4 +1,5 @@
 import Blob from './Blob.jsx'
+import Navbar from './Navbar.jsx'
 
 const Background = (props) => {
 
@@ -14,12 +15,15 @@ const Background = (props) => {
   return (
     <>
       {/* Background */}
-      <div className='bg-blue-lightest relative min-h-screen w-full -z-50'>
+      <div className='bg-blue-lightest absolute min-h-screen w-full -z-50'>
         {/* Blob */}
-        <div className='absolute -z-40 left-0 top-0 -translate-y-1/4 -translate-x-1/2'>
+        <div className='absolute -z-40 -left-0 top-0 -translate-y-1/4 -translate-x-1/2'>
           <Blob />
         </div>
 
+        {/* Navbar */}
+        <Navbar />
+        
         { children }
       </div>
     </>
