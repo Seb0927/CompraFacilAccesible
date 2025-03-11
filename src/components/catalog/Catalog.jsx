d459929a4390b8ceeffae4fdcf1ee9e2dbec0eaf
Seb0927 <sebastianidrobo@hotmail.com>
2025-03-10 20:03:35 -0500

    docs(catalog): justification to why not ul-li tags on articles

--- src/components/catalog/Catalog.jsx
+++ src/components/catalog/Catalog.jsx
@@ -73,9 +73,13 @@ const Catalog = () => {
 
   return (
   <section>
-    <h1 className='text-6xl text-blue-darkest pb-8 text-center font-bold'>Catálogo</h1>
     { /*(Section) Justification:
-      https://developer.mozilla.org/en-US/docs/Web/HTML/Element/section#usage_notes */}
+    https://developer.mozilla.org/en-US/docs/Web/HTML/Element/section#usage_notes */}
+    <h1 className='text-6xl text-blue-darkest pb-8 text-center font-bold'>Catálogo</h1>
+
+    {/* ¿Why not ul-li tags? TL;DR: Think a list as you always do, short elements and that's it
+    https://stackoverflow.com/questions/16213214/is-it-a-good-practice-to-put-articles-inside-lis
+    Posdata: Actually, there is a debate: https://stackoverflow.com/questions/19307443/semantic-html-of-an-articles-list */}
     <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8'>
       {products.map((product, index) => (
         <Item
