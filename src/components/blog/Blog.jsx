095871fcfb2d1b53c6588e36ebffa80c840feecf
Seb0927 <sebastianidrobo@hotmail.com>
2025-03-17 19:27:23 -0500

    feat(blog): create blog component

---
+++ src/components/blog/Blog.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <section>
+      <h1 className='text-6xl text-blue-darkest pb-8 text-center font-bold'>Blog</h1>
+      <div className='flex flex-col space-x-5'>
+        <p>Insertar noticias aquí</p>
+      </div>
+    </section>
+  )
+}
+
+export default Blog
