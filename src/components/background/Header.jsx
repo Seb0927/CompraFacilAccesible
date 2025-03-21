f09791c73c93e11284dc4e7c92ca504b72c14dae
Seb0927 <sebastianidrobo@hotmail.com>
2025-03-20 19:21:10 -0500

    feat(login): add login component

--- src/components/background/Header.jsx
+++ src/components/background/Header.jsx
@@ -1,22 +1,23 @@
 import { useContext } from 'react'
-import { UserContext } from '../../contexts/UserContext';
-import shoppingCartSvg from '../../assets/vectors/shopping_cart.svg';
-import iconPng from '../../assets/images/icon.png';
+import { UserContext } from '@/contexts/UserContext'
+import shoppingCartSvg from '../../assets/vectors/shopping_cart.svg'
+import iconPng from '../../assets/images/icon.png'
 
 const Header = () => {
-
   const { user, setUser } = useContext(UserContext);
+
+  const handleLogout = () => {
+    setUser(null);
+  };
 
   return (
     <header className='fixed bg-blue-darkest h-16 w-full z-50'>
       <div className='flex items-center h-full px-4 py-3 md:space-x-5'>
         <img src={iconPng} alt='Logo de CompraFacil' className='h-full md:block hidden' />
-        {/* Justification: 
-      https://developer.mozilla.org/es/docs/Web/HTML/Element/span*/}
         <span className='text-white text-xl font-bold md:block hidden'>CompraFácil</span>
+        <hr className='h-full w-0.5 blue bg-white md:block hidden' />
         {/* Justification:
       https://www.w3.org/WAI/WCAG22/Techniques/aria/ARIA11.html */}
-        <hr className='h-full w-0.5 blue bg-white md:block hidden' />
         <nav>
           <ul className='flex items-center space-x-4'>
             <li className='text-white text-xl'>
@@ -33,21 +34,22 @@ const Header = () => {
       </div>
 
       <div className='absolute right-0 top-0 h-full flex items-center px-4 py-3 space-x-5'>
-        {/* Justification:
-        https://stackoverflow.com/questions/21625672/what-is-an-accessible-way-to-mark-up-buttons-whose-only-content-is-an-icon */}
         <a href="/shopping-cart" className='h-full'>
           <img src={shoppingCartSvg} alt='Tu carrito de compras' className='h-full' />
         </a>
 
-        {user !== null ?
+        {user ? (
           <button
-            onClick={() => setUser(null)}
-            className='bg-blue-light h-full px-3 font-semibold text-lg rounded-md'>
+            onClick={handleLogout}
+            className='bg-blue-light h-full px-3 font-semibold text-lg rounded-md hover:bg-blue-medium-light'>
             Cerrar sesión
           </button>
-          :
-          <a href='/login' className='flex items-center justify-center bg-blue-light h-full px-3 font-semibold text-lg rounded-md hover:bg-blue-medium-light'>Iniciar sesión</a>}
-
+        ) : (
+          <a href='/login'
+            className='flex items-center justify-center bg-blue-light h-full px-3 font-semibold text-lg rounded-md hover:bg-blue-medium-light'>
+            Iniciar sesión
+          </a>
+        )}
       </div>
     </header>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, useParams } from "react-router";
 import './index.css'
-import { Background, Catalog, Blog, Post1, Post2, Post3 } from './components'
+import { Background, Catalog, Blog, Login, Post1, Post2, Post3 } from './components'
 
 // Component map for dynamic loading
 const components = {
   catalog: Catalog,
   blog: Blog,
+  login: Login,
   post1: Post1,
   post2: Post2,
   post3: Post3
@@ -38,6 +39,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<DynamicPage componentName="catalog" />} />
         <Route path="/blog" element={<DynamicPage componentName="blog" />} />
         <Route path="/blog/:componentName" element={<DynamicPage />} />
+        <Route path="/login" element={<DynamicPage componentName="login" />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
