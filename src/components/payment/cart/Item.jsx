2a3902c7d2054c5c1eecbb9ab289fbdcab1507df
Seb0927 <sebastianidrobo@hotmail.com>
2025-03-31 18:35:06 -0500

    fix(cart/Item): remove unnecesary div on img element

--- src/components/payment/cart/Item.jsx
+++ src/components/payment/cart/Item.jsx
@@ -18,13 +18,12 @@ const Item = (props) => {
   return (
     <div className='flex flex-row w-full space-x-6 items-center py-4 border-b-2 border-black first:border-t-2'>
       {/* Product image */}
-      <div className='flex-shrink-0'>
-        <img 
-          src={imageUrl + product.images[0]} 
-          alt={product.description || `Imagen de ${product.title}`} 
-          className='w-24 h-24 aspect-square object-cover rounded-sm' 
-        />
-      </div>
+      <img
+        src={imageUrl + product.images[0]}
+        alt={product.description || `Imagen de ${product.title}`}
+        className='w-24 h-24 aspect-square object-cover rounded-sm'
+      />
+
 
       {/* Product title */}
       <div className='flex-1'>
@@ -33,19 +32,19 @@ const Item = (props) => {
 
       {/* Quantity controls */}
       <div className='flex flex-row items-center space-x-4 flex-1 justify-center'>
-        <button 
+        <button
           onClick={() => decrementQuantity(product.title)}
           className='bg-blue-dark text-white h-min w-min p-1 hover:bg-blue-darkest'
           aria-label={`Disminuir cantidad de ${product.title}`}
         >
           <Minus className='h-4 w-4 fill-white' />
         </button>
-        
+
         <span className='text-xl' aria-label={`Cantidad: ${product.quantity}`}>
           {product.quantity}
         </span>
-        
-        <button 
+
+        <button
           onClick={() => incrementQuantity(product.title)}
           className='bg-blue-dark text-white h-min w-min p-1 hover:bg-blue-darkest'
           aria-label={`Aumentar cantidad de ${product.title}`}
@@ -63,7 +62,7 @@ const Item = (props) => {
 
       {/* Remove button */}
       <div className='flex-1 text-right'>
-        <button 
+        <button
           onClick={() => removeFromCart(product.title)}
           className='bg-blue-dark text-white py-2 px-4 hover:bg-blue-darkest'
           aria-label={`Eliminar ${product.title} del carrito`}
