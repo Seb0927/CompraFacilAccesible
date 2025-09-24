54dc313f77340c2c5189d77420f9d6f992416f69
Seb0927 <sebastianidrobo@hotmail.com>
2025-09-24 15:00:32 -0500

    fix(Catalog): implement notification on catalog component

--- src/components/catalog/Catalog.jsx
+++ src/components/catalog/Catalog.jsx
@@ -1,37 +1,48 @@
+import { useState } from 'react';
+
 import products from '@/utils/products';
 
 import Item from './Item';
 import Music from './Music';
+import Notification from './Notification';
 import { ShoppingCartProvider } from '../../contexts/ShoppingCartContext';
 
 const Catalog = () => {
+  const [notification, setNotification] = useState('');
+
+  const showNotification = (message) => setNotification(message);
+
   return (
-    <section>
-      <div className='flex flex-col items-center md:items-start mb-4 md:mb-0'>
-        <Music />
-      </div>
+    <>
+      <section>
+        <div className='flex flex-col items-center md:items-start mb-4 md:mb-0'>
+          <Music />
+        </div>
 
-      { /*(Section) Justification:
+        { /*(Section) Justification:
     https://developer.mozilla.org/en-US/docs/Web/HTML/Element/section#usage_notes */}
-      <h1 className='text-6xl text-blue-darkest pb-8 text-center font-bold'>Catálogo</h1>
+        <h1 className='text-6xl text-blue-darkest pb-8 text-center font-bold'>Catálogo</h1>
 
-      {/* ¿Why not ul-li tags? TL;DR: Think a list as you always do, short elements and that's it
+        {/* ¿Why not ul-li tags? TL;DR: Think a list as you always do, short elements and that's it
     https://stackoverflow.com/questions/16213214/is-it-a-good-practice-to-put-articles-inside-lis
     Posdata: Actually, there is a debate: https://stackoverflow.com/questions/19307443/semantic-html-of-an-articles-list */}
-      <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8'>
-        <ShoppingCartProvider>
-          {products.map((product, index) => (
-            <Item
-              key={index}
-              description={product.description}
-              images={product.images}
-              price={product.price}
-              title={product.title}
-            />
-          ))}
-        </ShoppingCartProvider>
-      </div>
-    </section>
+        <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8'>
+          <ShoppingCartProvider>
+            {products.map((product, index) => (
+              <Item
+                key={index}
+                description={product.description}
+                images={product.images}
+                price={product.price}
+                showNotification={showNotification}
+                title={product.title}
+              />
+            ))}
+          </ShoppingCartProvider>
+        </div>
+      </section>
+      <Notification message={notification} onClose={() => setNotification('')} />
+    </>
   )
 }
 
--- src/components/catalog/Item.jsx
+++ src/components/catalog/Item.jsx
@@ -1,15 +1,12 @@
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 
 import { ShoppingCartContext } from '@/contexts/ShoppingCartContext';
 
 import Carousel from './Carousel';
-import Notification from './Notification';
 
 const Item = (props) => {
-  const { title, price, description, images } = props;
+  const { title, price, description, images, showNotification } = props;
   const { addToCart } = useContext(ShoppingCartContext);
-
-  const [notification, setNotification] = useState('');
 
   const formattedPrice = new Intl.NumberFormat('es-CO', {
     style: 'currency',
@@ -19,30 +16,27 @@ const Item = (props) => {
 
   const handleAddToCart = () => {
     addToCart({ title, price, description, images });
-    setNotification(`${title} añadido al carrito`);
+    showNotification(`${title} añadido al carrito`);
   };
 
   return (
-    <>
-      <article className='h-96 md:h-88 flex bg-blue-medium-light p-6 rounded-lg items-center'>
-        <div className='h-3/4 w-full px-2 flex'>
-          <Carousel description={description} images={images} />
-          <div className='w-1/2 flex flex-row items-center justify-center'>
-            <div className='ml-6 w-full flex flex-col items-center'>
-              <h2 className='text-center font-bold text-xl'>{title}</h2>
-              <span className='text-center text-xl mt-2'>{formattedPrice}</span>
-              <button 
-                aria-label={`Añadir ${title}`}
-                className='bg-blue-dark text-white mt-3 lg:mt-6 px-6 py-2 text-xl w-full rounded-lg hover:bg-blue-darkest'
-                onClick={handleAddToCart}>
-                Añadir
-              </button>
-            </div>
+    <article className='h-96 md:h-88 flex bg-blue-medium-light p-6 rounded-lg items-center'>
+      <div className='h-3/4 w-full px-2 flex'>
+        <Carousel description={description} images={images} />
+        <div className='w-1/2 flex flex-row items-center justify-center'>
+          <div className='ml-6 w-full flex flex-col items-center'>
+            <h2 className='text-center font-bold text-xl'>{title}</h2>
+            <span className='text-center text-xl mt-2'>{formattedPrice}</span>
+            <button
+              aria-label={`Añadir ${title}`}
+              className='bg-blue-dark text-white mt-3 lg:mt-6 px-6 py-2 text-xl w-full rounded-lg hover:bg-blue-darkest'
+              onClick={handleAddToCart}>
+              Añadir
+            </button>
           </div>
         </div>
-      </article>
-      <Notification message={notification} onClose={() => setNotification('')} />
-    </>
+      </div>
+    </article>
   );
 };
 
